31d47624d722feda50eb020800cd96322fdd5a32
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-04-04 15:29:05 +0200

    Add authorized access to endpoints
    - before almost every endpoint, it checks if the email from the jwt token matches the requested id in the endpoint, making it so that only the user from the jwt can check their own demands
    - when approving demands, it checks if the user from the jwt is the one approving the demand

--- backend/IB-projekat/src/main/java/ib/projekat/IBprojekat/entity/CertificateDemandEntity.java
+++ backend/IB-projekat/src/main/java/ib/projekat/IBprojekat/entity/CertificateDemandEntity.java
@@ -9,7 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "certificate_requests")
+@Table(name = "certificate_demands")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
